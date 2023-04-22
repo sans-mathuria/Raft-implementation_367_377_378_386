fbecdf6496c02c92f437aaa38bfb3c77a12d7923
sanjanaswaminathan <83358000+sanjanaswaminathan@users.noreply.github.com>
2023-04-22 21:12:05 +0530

    Update raft_leader_logic.go

--- raft_leader_logic.go
+++ raft_leader_logic.go
@@ -108,6 +108,12 @@ func (this *RaftNode) broadcastHeartbeats() {
 						//-------------------------------------------------------------------------------------------/
 						// TODO
 						//-------------------------------------------------------------------------------------------/
+						this.nextIndex[peerId] += len(entries)
+						// this.matchIndex[peerId] = this.nextIndex[peerId] - 1	
+						this.matchIndex[peerId] = (this.nextIndex[peerId] - 1) - len(entries)	
+						if this.matchIndex[peerId] < this.commitIndex {	
+							this.matchIndex[peerId] = this.commitIndex	
+						}	
 
 						if (aeType == "Heartbeat" && LogHeartbeatMessages) || aeType == "AppendEntries" {
 							this.write_log("%s reply from NODE %d success: nextIndex := %v, matchIndex := %v", aeType, peerId, this.nextIndex, this.matchIndex)
@@ -125,12 +131,12 @@ func (this *RaftNode) broadcastHeartbeats() {
 								matchCount := 1 // Leader itself
 
 								for _, peerId := range this.peersIds {
-									if { // TODO  // When should you update matchCount?
+									if this.matchIndex[peerId] >= i { // TODO  // When should you update matchCount?
 										matchCount++
 									}
 								}
 
-								if { // TODO  // When should you update commitIndex to i?
+								if matchCount > (len(this.peersIds)+1)/2 { // TODO  // When should you update commitIndex to i?
 									this.commitIndex = i
 								}
 							}
@@ -152,7 +158,7 @@ func (this *RaftNode) broadcastHeartbeats() {
 						//-------------------------------------------------------------------------------------------/
 						// TODO
 						//-------------------------------------------------------------------------------------------/
-
+						this.nextIndex[peerId] = currentPeer_nextIndex - len(entries)
 						if (aeType == "Heartbeat" && LogHeartbeatMessages) || aeType == "AppendEntries" {
 							this.write_log("%s reply from NODE %d was failure; Hence, decrementing its nextIndex", aeType, peerId)
 						}
